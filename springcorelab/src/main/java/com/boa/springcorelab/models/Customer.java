2fcff50d0ecfd79acbf1af7f89853bafbd2af273
eswaribala <parameswaribala@gmail.com>
2022-01-03 12:39:33 +0530

    updated

--- springcorelab/src/main/java/com/boa/springcorelab/models/Customer.java
+++ springcorelab/src/main/java/com/boa/springcorelab/models/Customer.java
@@ -2,13 +2,17 @@ package com.boa.springcorelab.models;
 
 import java.time.LocalDate;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Customer {
 	
 	private long customerId;
 	private String name;
-	private LocalDate dob;
+	//private LocalDate dob;
 	private String email;
 
 }
