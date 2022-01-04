cbc4b9b6d910c14d027f7d0db9f5c2ce4862d19b
eswaribala <parameswaribala@gmail.com>
2022-01-04 11:31:33 +0530

    updated

--- inventoryservice/src/main/java/com/boa/inventoryservice/controllers/CategoryController.java
+++ inventoryservice/src/main/java/com/boa/inventoryservice/controllers/CategoryController.java
@@ -1,9 +1,32 @@
 package com.boa.inventoryservice.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.boa.inventoryservice.models.Category;
+import com.boa.inventoryservice.services.CategoryService;
+
 @RestController
+@RequestMapping("/categories")
 public class CategoryController {
+    @Autowired  
+	private CategoryService categoryService;    
+    
+    @PostMapping({"/v1.0", "/v1.1"})
+    public ResponseEntity<?> addCategory(@RequestBody Category category){
+    	Category categoryObj=this.categoryService.addCategory(category);
+    	if(categoryObj!=null)
+    		return ResponseEntity.status(HttpStatus.ACCEPTED).body(categoryObj);
+    	else
+    		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Category Not saved");
+    	
+    }
+    
 
 	
 }
