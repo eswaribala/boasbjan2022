610efb818441579e3cf49a0c079043f396bfefd2
eswaribala <parameswaribala@gmail.com>
2022-01-04 16:09:31 +0530

    updated

--- inventoryservice/src/main/java/com/boa/inventoryservice/filters/InventoryFilter.java
+++ inventoryservice/src/main/java/com/boa/inventoryservice/filters/InventoryFilter.java
@@ -1,6 +1,7 @@
 package com.boa.inventoryservice.filters;
 
 import java.io.IOException;
+import java.util.Random;
 
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
@@ -10,9 +11,11 @@ import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.boot.ExitCodeGenerator;
+
 import com.boa.inventoryservice.exceptions.JwtTokenMissingException;
 
-public class InventoryFilter implements Filter  {
+public class InventoryFilter implements Filter,ExitCodeGenerator  {
 
 	@Override
 	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain)
@@ -26,11 +29,18 @@ public class InventoryFilter implements Filter  {
 		String header = httpRequest.getHeader("Authorization");
 
 		if (header == null || !header.startsWith("Bearer")) {
-			throw new JwtTokenMissingException("No JWT token found in the request headers");
+			throw new JwtTokenMissingException("No JWT token found in the "
+					+ "request headers"+this.getExitCode());
 		}
 
 		chain.doFilter(httpRequest, httpResponse);
 		
+	}
+
+	@Override
+	public int getExitCode() {
+		// TODO Auto-generated method stub
+		return new Random().nextInt(100000);
 	}
 
 }
