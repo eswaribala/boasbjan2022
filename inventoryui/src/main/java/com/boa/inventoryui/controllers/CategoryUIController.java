b934c90e42a2288b7825fcf3300753151d8017bb
eswaribala <parameswaribala@gmail.com>
2022-01-04 14:58:03 +0530

    updated

--- inventoryui/src/main/java/com/boa/inventoryui/controllers/CategoryUIController.java
+++ inventoryui/src/main/java/com/boa/inventoryui/controllers/CategoryUIController.java
@@ -5,11 +5,16 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.client.RestTemplate;
 
 import com.boa.inventoryui.models.Category;
@@ -37,6 +42,22 @@ public class CategoryUIController {
 	@GetMapping("/add")
 	public String addCategory() {
 		return "addcategory";
+	}
+	
+	
+	@PostMapping("/categories")
+	public String addWinner(@ModelAttribute("category") Category category) 
+	{
+		
+		  HttpHeaders headers = new HttpHeaders();
+		  headers.setContentType(MediaType.APPLICATION_JSON); 
+		  HttpEntity<Category> request = new HttpEntity<>(category,headers); 
+		  ResponseEntity<?> serviceResponse=restTemplate. postForEntity(serviceUrl+"/categories/v1.0",request,
+		  Category.class);
+		 
+	
+		return "redirect:/";
+
 	}
 	
 }
