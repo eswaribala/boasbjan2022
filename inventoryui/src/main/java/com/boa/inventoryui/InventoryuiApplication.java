a2fec72398a720f168aaef455d74053ca59a1ca6
eswaribala <parameswaribala@gmail.com>
2022-01-04 12:51:35 +0530

    updated

---
+++ inventoryui/src/main/java/com/boa/inventoryui/InventoryuiApplication.java
@@ -0,0 +1,13 @@
+package com.boa.inventoryui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventoryuiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventoryuiApplication.class, args);
+	}
+
+}
